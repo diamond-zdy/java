79605bb8b27bbc56e70f36020ae8fc53a9ec2a2e
diamond <1181352644@qq.com>
2019-11-24 19:54:32 +0800

    图书管理系统图书部分

---
+++ 11.24/src/BookList.java
@@ -0,0 +1,23 @@
+public class BookList {
+    private Book[] books = new Book[10];
+    private int usedSize;
+    public BookList(){
+        books[0] = new Book("三国演义","罗贯中",22,"小说");
+        books[1] = new Book("水浒传","施耐庵",54,"小说");
+        books[1] = new Book("西游记","吴承恩",85,"小说");
+        this.usedSize = 3;
+    }
+    public void setBooks(int pos,Book book){
+        books[pos] = book;
+    }
+    public Book getBook(int pos){
+        return books[pos];
+    }
+    public int getUsedSize() {
+        return usedSize;
+    }
+
+    public void setUsedSize(int usedSize) {
+        this.usedSize = usedSize;
+    }
+}
