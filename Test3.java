57d39540ded93fea51b73b2ab8596680f7269dae
diamond <1181352644@qq.com>
2019-12-02 16:46:55 +0800

    将一句话按单词逆置

---
+++ Test3.java
@@ -0,0 +1,36 @@
+public class Test3 {
+    public static void reverse(char[] array,int left,int right) {
+        while (left < right) {
+            char tmp = array[left];
+            array[left] = array[right];
+            array[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+
+    public static String reverseSentence(String str) {
+        char[] array = str.toCharArray();
+        reverse(array,0,array.length-1);
+        int i = 0;
+        int j = 0;
+        //单词逆置
+        while (i < array.length) {
+            if (array[i] == ' '){
+                i++;
+                j++;
+            }else if (j == array.length||array[j] == ' '){
+                reverse(array,i,j-1);
+                i = ++j;
+            }else {
+                j++;
+            }
+        }
+        return String.copyValueOf(array);
+    }
+
+    public static void main(String[] args) {
+        String ret = reverseSentence("i am student");
+        System.out.println(ret);
+    }
+}
