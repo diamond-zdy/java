8a08f04960b168015b387e13ce314d9c2d1f747a
diamond <1181352644@qq.com>
2020-02-13 17:02:24 +0800

    OSMonitor练习

---
+++ 20200213/src/com/bitedu/gui/OSmonitorController.java
@@ -0,0 +1,13 @@
+package com.bitedu.gui;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
+public class OSmonitorController {
+
+    public void handleCPUSelectionChanged(Event event) {
+    }
+
+    public void handleSelectFile(ActionEvent actionEvent) {
+    }
+}
