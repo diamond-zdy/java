eac5dc65dd99919f3ff83297967562eb04e31cd3
diamond <1181352644@qq.com>
2020-05-22 22:48:33 +0800

    注册，登录，商品上架，下架功能实现

---
+++ cash1/src/main/java/entity/OrderItem.java
@@ -0,0 +1,21 @@
+package entity;
+
+public class OrderItem {
+    private Integer id;
+    private String orderId;
+    private Integer goodsId;
+    private String goodsName;
+    private String goodsIntroduce;
+    private Integer goodsNum;
+    private String goodsUnit;
+    private Integer goodsPrice;
+    private Integer goodsDiscount;
+
+    public double getGoodsPrice(){
+        return goodsPrice * 1.0 / 100;
+    }
+
+    public int getGoodsPriceInt(){
+        return goodsPrice;
+    }
+}
